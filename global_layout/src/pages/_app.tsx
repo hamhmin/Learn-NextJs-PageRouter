238f1a6377688227926d31525c5babdfac1341a7
hhm <hamhmin1031@gmail.com>
2025-06-28 18:01:50 +0900

    글로벌 레이아웃 설정

---
+++ global_layout/src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import GlobalLayout from "@/components/global-layout";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalLayout>
+        <Component {...pageProps} />
+      </GlobalLayout>
+    </>
+  );
+}
---
+++ global_layout/src/pages/index.tsx
@@ -0,0 +1,15 @@
+// import "./index.css"; // nextjs는 app 컴포넌트가 아닌 곳에서 css파일을 호출하는것을 막는다.
+
+// CSS Module
+import style from "./index.module.css";
+// CSS Module을 사용하면 불러올 수 있으며
+// className에 불러온 style.(css에서 지정한 선택자)로 작성하면 다른 컴포넌트와 중복되지않게 알아서 적용한다.
+
+export default function Home() {
+  return (
+    <>
+      <h1 className={style.h1}>index</h1>
+      <h2 className={style.h2}>H2</h2>
+    </>
+  );
+}
