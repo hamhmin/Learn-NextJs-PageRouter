33cb7f72db8864ddca86df95922c9858d46fcdec
hhm <hamhmin1031@gmail.com>
2025-06-28 18:33:05 +0900

    페이지별 레이아웃 설정

--- global_layout/src/pages/_app.tsx
+++ global_layout/src/pages/_app.tsx
@@ -1,13 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import GlobalLayout from "@/components/global-layout";
+import { ReactNode } from "react";
+import { NextPage } from "next";
 
-export default function App({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & { Component: NextPageWithLayout }) {
+  // getLayout이 없는 컴포넌트의 경우 page 그대로 리턴
+  const getLayout =
+    Component.getLayout ??
+    ((page: ReactNode) => {
+      return page;
+    });
   return (
     <>
-      <GlobalLayout>
-        <Component {...pageProps} />
-      </GlobalLayout>
+      <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>
     </>
   );
 }
--- global_layout/src/pages/index.tsx
+++ global_layout/src/pages/index.tsx
@@ -1,7 +1,9 @@
 // import "./index.css"; // nextjs는 app 컴포넌트가 아닌 곳에서 css파일을 호출하는것을 막는다.
 
 // CSS Module
+import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
+import { ReactNode } from "react";
 // CSS Module을 사용하면 불러올 수 있으며
 // className에 불러온 style.(css에서 지정한 선택자)로 작성하면 다른 컴포넌트와 중복되지않게 알아서 적용한다.
 
@@ -13,3 +15,9 @@ export default function Home() {
     </>
   );
 }
+
+// hetLayout으로 감싸주는게 가능함
+// Home에서만 서치인풋 컴포넌트를 적용한 사례.
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
