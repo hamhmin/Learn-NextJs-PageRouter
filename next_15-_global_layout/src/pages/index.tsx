f33c1a3e0a7f02bb5a9d209ba54ed5e4025a630f
hhm <hamhmin1031@gmail.com>
2025-07-06 15:02:02 +0900

    learn ssr 01

--- next_15-_global_layout/src/pages/index.tsx
+++ next_15-_global_layout/src/pages/index.tsx
@@ -3,24 +3,56 @@
 // CSS Module
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 // CSS Module을 사용하면 불러올 수 있으며
 // className에 불러온 style.(css에서 지정한 선택자)로 작성하면 다른 컴포넌트와 중복되지않게 알아서 적용한다.
 
-import books from '@/mock/books.json'
+import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
+export const getServerSideProps = () => {
+  //  getServerSideProps == 약속된 이름의 함수, ssr로 작동함.
+  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+  // return 값 방식도 프레임워크에 의해 강제된다.
+  // 서버에서만 실행되기에 console.log가 브라우저에서 보이지 않음, window.location 과 같은 브라우저를 읽는 js 사용 불가함.
+  // window.location;
+  console.log("서버");
+  const data = "hello";
+  return {
+    props: {
+      data,
+    },
+  };
+};
 
-export default function Home() {
+export default function Home(
+  // type은 InferGetServerSidePropsType를 불러와 사용하고, getServerSideProps의 타입을 추론하게 한다.
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  // ssr 방식으로 작동하도록 설정했기때문에 getServerSideProps함수 외의 해당 컴포넌트에서도
+  // 브라우저를 읽는 window 코드를 사용할 수 없다.
+  // console.log(window);
+
+  // 컴포넌트가 마운트 되는 시점부터는 브라우저영역이기에 window 사용이 가능하다.
+  useEffect(() => {
+    console.log(window);
+  }, []);
+
+  console.log(props.data);
   return (
-    <div  className={style.container}>
+    <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book)=><BookItem key={book.id}{...book}/>)}
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book)=><BookItem key={book.id}{...book}/>)}
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
       </section>
     </div>
   );
