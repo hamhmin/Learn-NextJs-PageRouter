325280224dd5581f35effdfef236b8639391bf37
hhm <hamhmin1031@gmail.com>
2026-03-14 15:49:36 +0900

    5.3) 풀 라우트 캐시 3. 동적 경로에 적용하기

--- next_app_router/src/app/(with-searchbar)/search/page.tsx
+++ next_app_router/src/app/(with-searchbar)/search/page.tsx
@@ -1,4 +1,3 @@
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 
@@ -10,6 +9,7 @@ export default async function Page({
   const { q } = await searchParams;
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
+    { cache: "force-cache" },
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다... search/page.tsx</div>;
--- next_app_router/src/app/book/[id]/page.tsx
+++ next_app_router/src/app/book/[id]/page.tsx
@@ -1,4 +1,16 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
+
+export const dynamicParams = false; // 설정시 모든 접속에서 원치않는 static 페이지가 생성되는 현상을 막아줌.
+
+// ssg의 getStaticProps fallback:blocking, getStaticPaths 설정하는거랑 같음.
+// 하지만 풀라우터캐시에 저장되기때문에 generateStaticParams로 return한 id를 포함한 페이지와 첫 접속 이후 생성된 페이지들은 데이터 패칭을 하는게 아니라 풀라우터캐시에서 가져오기때문에 그 (데이터패칭을 포함한 사전렌더링단계)다음단계를 가지않아서 데이터패칭을 할 필요가 없어지기 때문에 데이터패칭을 안하는거임.
+
+// 이때 "fetch 옵션에 "force-cache"를 설정하지 않았음에도 어떻게 이럴 수 있느냐"고 질문 주셨는데요
+// 동적 경로를 갖는 페이지에 한해 예외적으로! generateStaticParams 함수가 존재한다면, 무조건 Static 페이지로 설정하게 됩니다.
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
 
 export default async function Page({
   params,
@@ -10,6 +22,9 @@ export default async function Page({
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`,
   );
   if (!response.ok) {
+    if (response.status === 404) {
+      notFound(); //  없는 페이지. 404일때 notFound()
+    }
     return <div>오류가 발생했습니다... book/page.tsx</div>;
   }
   const bookData = await response.json();
