cb2c2aa084f98414699611b04458e214e7f2a4e3
hhm <hamhmin1031@gmail.com>
2026-03-15 03:27:17 +0900

    5.4) 라우트 세그먼트 옵션

--- next_app_router/src/app/(with-searchbar)/search/page.tsx
+++ next_app_router/src/app/(with-searchbar)/search/page.tsx
@@ -1,5 +1,10 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
+// export const dynamic = "force-static";
+// dynamic 페이지를 static으로 만들면 ssg로 되기때문에 fetch 값이 undefined가 됨. 제대로 작동 안할 수 있음.
+
+// export const dynamic = "error";
+// 4. error : 페이지를 강제로 Static 페이지 설정 (설정하면 안되는 이유를 -> 빌드시 오류 발생시킴)
 
 export default async function Page({
   searchParams,
