cea80f8d8af238ff56ebe7b634eb13a70926b234
hhm <hamhmin1031@gmail.com>
2026-03-18 18:20:37 +0900

    7.6) 클라이언트 컴포넌트에서의 서버액션

--- next_app_router/src/actions/create-review.action.ts
+++ next_app_router/src/actions/create-review.action.ts
@@ -1,23 +1,30 @@
 "use server";
+import { delay } from "@/util/delay";
 import { revalidateTag } from "next/cache";
 import { revalidatePath } from "next/cache";
 
 // 서버에서만 실행됨.
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(state: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
   // const author: FormDataEntryValue | null 라서 ?.toString() 으로 string|undefined로 추론하게 만든다.
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과ㅏ 작성자를 입력해주세요.",
+    };
   }
   try {
+    await delay(2000);
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       { method: "POST", body: JSON.stringify({ bookId, content, author }) },
     );
     // console.log(response.status);
-
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     // 1. 특정 주소의 해당하는 페이지만 재검증
     // revalidatePath(`/book/${bookId}`);
     // 이 페이지를 재검증함. 서버에서만 호출 가능. 캐시도 무효화시켜버림. 풀라우트캐시까지.
@@ -34,8 +41,11 @@ export async function createReviewAction(formData: FormData) {
 
     // 5. 태그 기준, 데이터 캐시 재검증
     revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return { status: false, error: `리뷰 저장에 실패했습니다 : ${err}` };
   }
 }
--- next_app_router/src/components/review-editor.tsx
+++ next_app_router/src/components/review-editor.tsx
@@ -1,16 +1,56 @@
+"use client";
 import style from "./review-editor.module.css";
 
 import { createReviewAction } from "@/actions/create-review.action";
+import { useActionState, useEffect } from "react";
 
 export function ReviewEditor({ bookId }: { bookId: string }) {
+  //useActionState는 클라이언트 컴포넌트에서 서버 액션의 진행 파악, 에러핸들링을 할 수 있게해줌.
+
+  const [state, formAction, isPending] = useActionState(
+    createReviewAction,
+    null,
+  );
+  /*
+  아래에서 return하는게 state에 들어감.
+  if (!bookId || !content || !author) {
+    return {
+      status: false,
+      error: "리뷰 내용과ㅏ 작성자를 입력해주세요.",
+    };
+  }
+*/
+  /*
+useActionState를 사용하면 createReviewAction의 매개변수를 앞쪽에 state를 받아야줘야함.
+export async function createReviewAction(state: any, formData: FormData) {
+
+*/
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
   return (
     <section>
-      <form className={style.form_container} action={createReviewAction}>
+      <form className={style.form_container} action={formAction}>
         <input name="bookId" value={bookId} hidden readOnly />
-        <textarea required name="content" placeholder="리뷰 내용" />
+        <textarea
+          disabled={isPending}
+          required
+          name="content"
+          placeholder="리뷰 내용"
+        />
         <div className={style.submit_container}>
-          <input required name="author" placeholder="작성자" />
-          <button type="submit">작성하기</button>
+          <input
+            disabled={isPending}
+            required
+            name="author"
+            placeholder="작성자"
+          />
+          <button disabled={isPending} type="submit">
+            {isPending ? "로딩 중" : "작성하기"}
+          </button>
         </div>
       </form>
     </section>
